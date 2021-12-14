782a2fe8d3e7da9f28b11f8b85d4e9a59a559f39
VerniSharma <vernisharma015@gmail.com>
2021-12-13 22:13:13 -0800

    this is verni

---
+++ alexa.py/alexa.py
@@ -0,0 +1,28 @@
+import datetime
+
+import pyttsx3
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+print(voices[1].id)
+engine.setProperty('voice',voices[1].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+def wishme():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("good morning")
+    elif hour<=12 and hour<18:
+        speak("good afternoon")
+    else:
+        speak("good evening")
+    speak("i am alexa , how may i help you verni")
+
+
+
+if __name__ == '__main__':
+    wishme()
+
